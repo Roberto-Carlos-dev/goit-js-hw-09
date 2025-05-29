e9c15c99a05d713e3bd2db05349f804e3f706254
Roberto-Carlos-dev <mikulskiyroman@gmail.com>
2025-05-29 16:17:50 +0000

    Deploying to gh-pages from @ Roberto-Carlos-dev/goit-js-hw-09@e3b54b992e72d0fa93abc2dc180a18ca933d406c 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as o}from"./assets/vendor-CgTBfC_f.js";document.querySelector(".gallery");new o(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
